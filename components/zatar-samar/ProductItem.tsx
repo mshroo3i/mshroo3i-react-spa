8f264ea18b2f6ff3a012afcd7129ec19b91244f0
Mohammad Mohammad <mmohammadq8@outlook.com>
2021-12-16 01:59:04 +0300

    minor fixes

--- components/zatar-samar/ProductItem.tsx
+++ components/zatar-samar/ProductItem.tsx
@@ -13,8 +13,8 @@ export const ProductItem = ({ product, onClick, displayPrice = false }: { produc
   }
 
   return (
-      <button onClick={onClick} key={product.id} className="text-right mt-6 text-sm font-medium text-gray-500 border rounded border-gray-200 divide-y divide-gray-200">
-        <li key={product.id} className="grid grid-cols-12 p-2 space-x-3 space-x-reverse">
+      <button onClick={onClick} key={product.id} className="flex text-right mt-2 text-sm font-medium text-gray-500 border rounded border-gray-200">
+        <li key={product.id} className="grow grid grid-cols-12 p-2 space-x-3 space-x-reverse">
           {product.imageSrc && <div className="col-span-3"><Image
             src={product.imageSrc}
             alt={product.imageAlt}
@@ -24,11 +24,11 @@ export const ProductItem = ({ product, onClick, displayPrice = false }: { produc
             width={500}
             className="rounded-md"
           /></div>}
-          <div className={`flex-auto space-y-2 pt-4 ${productAndNameColSpan}`}>
+          <div className={`space-y-2 pt-4 ${productAndNameColSpan}`}>
             <h3 className="text-gray-900 font-medium">{product.name}</h3>
             <p>{product.description}</p>
           </div>
-          {displayPrice && <p className={`flex-none font-medium text-gray-900 py-4 text-left col-span-3`}>{(new Price(product.price)).toFormattedString()}</p>}
+          {displayPrice && <p className={`font-medium text-gray-900 py-4 text-left col-span-3`}>{(new Price(product.price)).toFormattedString()}</p>}
         </li>
         {/* <div className="absolute top-2 left-2 py-1 px-1 text-sm lg:text-base font-bold bg-white text-gray-800 rounded bg-opacity-75">{product.price.toFormattedString()}</div> */}
       </button>
