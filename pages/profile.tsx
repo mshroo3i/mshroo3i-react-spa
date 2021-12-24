0ad2859ac39a31dc59d12af101828a589e1946f9
Mohammad Mohammad <mmohammadq8@outlook.com>
2021-12-24 21:42:30 +0300

    some improvements

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import { UserProfile, withPageAuthRequired } from '@auth0/nextjs-auth0';
+import { Layout, siteTitle } from "../components/Layout";
 
 type ProfileProps = { user: UserProfile };
 
 export default function Profile({ user }: ProfileProps) {
   return (
     (
-      <div>
-        <img src={user.picture} alt={user.name} />
-        <h2>{user.name}</h2>
-        <p>{user.email}</p>
-        <code>{JSON.stringify(user, null, 2)}</code>
-      </div>
+      <Layout>
+        <div>
+          <img src={user.picture} alt={user.name} />
+          <h2>{user.name}</h2>
+          <p>{user.email}</p>
+          <code><pre>{JSON.stringify(user, null, 2)}</pre></code>
+        </div>
+      </Layout>
     )
   );
 }
